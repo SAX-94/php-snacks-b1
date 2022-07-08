fb89e029ad7677d99eef4dbfe23542c9955e827f
SAX_94 <matteosacchi94@gmail.com>
2022-07-08 17:47:47 +0200

    added matches array

---
+++ snack1.php
@@ -0,0 +1,36 @@
+<?php
+
+$matches = [
+    [
+        "homeTeam" => "Olimpia Milano",
+        "guestTeam" => "Pallacanestro Cantù",
+        "homeScore" => 55,
+        "guestScore" => 60
+    ],
+    [
+        "homeTeam" => "Orlando Magics",
+        "guestTeam" => "Boston Celtics",
+        "homeScore" => 90,
+        "guestScore" => 80
+    ],
+    [
+        "homeTeam" => "Pallacanestro Reggiana",
+        "guestTeam" => "Polisportiva Dinamo",
+        "homeScore" => 40,
+        "guestScore" => 70
+    ],
+    [
+        "homeTeam" => "Pallacanestro Brescia",
+        "guestTeam" => "New Basket Brindisi",
+        "homeScore" => 70,
+        "guestScore" => 60
+    ],
+    [
+        "homeTeam" => "Chicago Bulls",
+        "guestTeam" => "Charlotte Hornets",
+        "homeScore" => 120,
+        "guestScore" => 78
+    ],
+];
+
+?>
