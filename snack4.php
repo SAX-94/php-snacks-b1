1c97afa3ee7b5437eefe39387700e5d34b0f274e
SAX_94 <matteosacchi94@gmail.com>
2022-07-08 18:34:22 +0200

    added necessary HTML

--- snack4.php
+++ snack4.php
@@ -2,9 +2,23 @@
 
 $randomArray = [];
 
-while (count($randomArray) < 100) {
+while (count($randomArray) < 15) {
     $randArr = rand(0, 100);
     if (!in_array($randArr, $randomArray)) $randomArray[] = $randArr;
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random 15</title>
+</head>
+<body>
+    <h1>Array of random numbers (0 - 100):</h1>
+    <h2><?php for($i=0; $i<count($randomArray); $i++) echo "$randomArray[$i] | " ?></h2>
+</body>
+</html>
