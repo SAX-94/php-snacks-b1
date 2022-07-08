8930250b8e83ba6811ee329a5302f464772b64e3
SAX_94 <matteosacchi94@gmail.com>
2022-07-08 18:46:49 +0200

    added array with class details

---
+++ snack7.php
@@ -0,0 +1,46 @@
+<?php
+
+$classe = [
+    [
+        "nome" => "Elvira",
+        "cognome" => "Porcellato",
+        "voti" => [6, 7, 8, 4, 7]
+    ],
+    [
+        "nome" => "Valeria",
+        "cognome" => "Zamengo",
+        "voti" => [3, 9, 5, 5, 7]
+    ],
+    [
+        "nome" => "Chiara",
+        "cognome" => "Vecoli",
+        "voti" => [2, 5, 7, 6, 8]
+    ],
+    [
+        "nome" => "Ermenegildo",
+        "cognome" => "Filippelli",
+        "voti" => [9, 8, 7, 10, 8]
+    ],
+    [
+        "nome" => "Saverio",
+        "cognome" => "Franscini",
+        "voti" => [2, 4, 3, 6, 5]
+    ],
+    [
+        "nome" => "Germana",
+        "cognome" => "Vivaldi",
+        "voti" => [8, 7, 6, 7, 5]
+    ],
+    [
+        "nome" => "Federico",
+        "cognome" => "Donini",
+        "voti" => [8, 6, 6, 4, 7]
+    ],
+    [
+        "nome" => "Leopoldo",
+        "cognome" => "Tacchini",
+        "voti" => [9, 9, 8, 7, 8]
+    ],
+];
+
+?>
