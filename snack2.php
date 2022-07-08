02c89658a8c10f4b2baaa87eba61af27e7f12221
SAX_94 <matteosacchi94@gmail.com>
2022-07-08 18:09:58 +0200

    created PHP necessary code

--- snack2.php
+++ snack2.php
@@ -1,4 +1,21 @@
 <?php
 
+$name = $_GET["name"];
+$email = $_GET["email"];
+$age = $_GET["age"];
 
-?>
+function verifyAccess($name, $email, $age)
+{
+    if (
+        strlen($name) >= 3 &&
+        strpos($email, '.') !== false &&
+        strpos($email, '@') !== false &&
+        !empty($age) &&
+        is_numeric($age)
+    ) return "Accesso riuscito!";
+    else return "Accesso negato!";
+}
+
+var_dump($name);
+var_dump($email);
+var_dump($age);
