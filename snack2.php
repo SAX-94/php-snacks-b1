e12383f1ac7277e6324cc1a2a1a9a2202bd6a15d
SAX_94 <matteosacchi94@gmail.com>
2022-07-08 18:13:26 +0200

    added necessary HTML code

--- snack2.php
+++ snack2.php
@@ -21,3 +21,16 @@ var_dump($email);
 var_dump($age);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accesso</title>
+</head>
+<body>
+    <h1><?php echo verifyAccess($name, $email, $age); ?></h1>
+</body>
+</html>
