b01b14261c6175f43833165394e775c7eceaa145
SAX_94 <matteosacchi94@gmail.com>
2022-07-11 09:14:47 +0200

    added auto-formatting

--- snack2.php
+++ snack2.php
@@ -24,13 +24,16 @@ var_dump($age);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accesso</title>
 </head>
+
 <body>
     <h1><?php echo verifyAccess($name, $email, $age); ?></h1>
 </body>
+
 </html>
--- snack4.php
+++ snack4.php
@@ -11,14 +11,17 @@ while (count($randomArray) < 15) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Random 15</title>
 </head>
+
 <body>
     <h1>Array of random numbers (0 - 100):</h1>
-    <h2><?php for($i=0; $i<count($randomArray); $i++) echo "$randomArray[$i] | " ?></h2>
+    <h2><?php for ($i = 0; $i < count($randomArray); $i++) echo "$randomArray[$i] | " ?></h2>
 </body>
+
 </html>
